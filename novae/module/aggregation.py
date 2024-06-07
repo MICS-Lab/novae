a2f89cdf3ba0bf7aa4813f08f847028cf0bfb7b5
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2024-06-07 10:50:21 +0200

    minor aggr refactor

--- novae/module/aggregation.py
+++ novae/module/aggregation.py
@@ -8,8 +8,10 @@ from torch_geometric.nn.aggr import AttentionalAggregation
 class NodeAttentionAggregation(L.LightningModule):
     def __init__(self, out_channels: int):
         super().__init__()
-        self.seq = nn.Sequential(nn.Linear(out_channels, 1), nn.Sigmoid())
-        self.attention_aggregation = AttentionalAggregation(self.seq)
+        self.gate_nn = nn.Linear(out_channels, 1)
+        self.nn = nn.Linear(out_channels, out_channels)
+
+        self.attention_aggregation = AttentionalAggregation(gate_nn=self.gate_nn, nn=self.nn)
 
     def forward(self, x: Tensor, index: Tensor) -> Tensor:
         return self.attention_aggregation(x, index=index)
