26e01188e71aca6c096a53ccca28bb6e7b8ba396
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2024-06-24 17:20:50 +0200

    increase mem training

--- scripts/slurm/train.sh
+++ scripts/slurm/train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=novae
 #SBATCH --output=/mnt/beegfs/userdata/q_blampey/.jobs_outputs/%j
-#SBATCH --mem=40G
+#SBATCH --mem=256G
 #SBATCH --cpus-per-task=8
 #SBATCH --partition=gpgpuq
 #SBATCH --gres=gpu:a100:1
