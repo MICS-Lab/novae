123b4bd1b42077f5389c397420c3decf27a02868
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2024-07-18 14:45:46 +0200

    add ruche config

--- scripts/ruche/gpu.sh
+++ scripts/ruche/gpu.sh
@@ -14,7 +14,7 @@ source activate novae-gpu
 cd /gpfs/workdir/blampeyq/novae/scripts
 
 # Get config
-DEFAULT_CONFIG=all_3.yaml
+DEFAULT_CONFIG=all_6.yaml
 CONFIG=${1:-$DEFAULT_CONFIG}
 echo Running with CONFIG=$CONFIG
 
