4dd333b1ad413d92f89166151f1a5189fd530c4b
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2024-07-31 14:04:53 +0200

    download new xenium slides

---
+++ scripts/ruche/download.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=novae
+#SBATCH --output=/gpfs/workdir/blampeyq/.jobs_outputs/%j
+#SBATCH --mem=80G
+#SBATCH --cpus-per-task=8
+#SBATCH --partition=cpu_long
+
+#module purge
+#module load anaconda3/2022.10/gcc-11.2.0 && source activate novae
+#cd /gpfs/workdir/blampeyq/novae/data
+
+sh xenium_download.sh
