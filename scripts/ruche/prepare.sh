a0525d6f0fd2bb9e306d356a2be17cbb9581ce75
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2024-07-31 17:41:43 +0200

    add prepare

---
+++ scripts/ruche/prepare.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=novae
+#SBATCH --output=/gpfs/workdir/blampeyq/.jobs_outputs/%j
+#SBATCH --mem=80G
+#SBATCH --cpus-per-task=8
+#SBATCH --partition=cpu_long
+
+module purge
+module load anaconda3/2022.10/gcc-11.2.0 && source activate novae
+
+cd /gpfs/workdir/blampeyq/novae/data
+
+python -u prepare.py -n all2 -d xenium -u
