80971a5a7757961558e7c168b90475d2d83a7280
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2024-08-01 17:38:28 +0200

    MIN_PROTOTYPES_RATIO 0.2

--- novae/_constants.py
+++ novae/_constants.py
@@ -52,7 +52,7 @@ class Nums:
     SWAV_EPSILON: float = 0.05
     SINKHORN_ITERATIONS: int = 3
     QUEUE_SIZE: int = 5
-    MIN_PROTOTYPES_RATIO: float = 0.1
+    MIN_PROTOTYPES_RATIO: float = 0.2
 
     # misc nums
     MEAN_NGH_TH_WARNING: float = 3.5
