cd14f198cd9e21fedaf9daa6a00e15f394db9918
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2024-08-05 18:05:32 +0200

    N_OBS_THRESHOLD: int = 1_000

--- novae/_constants.py
+++ novae/_constants.py
@@ -56,4 +56,4 @@ class Nums:
 
     # misc nums
     MEAN_NGH_TH_WARNING: float = 3.5
-    N_OBS_THRESHOLD: int = 2_000_000  # above this number, lazy loading is used
+    N_OBS_THRESHOLD: int = 1_000  # above this number, lazy loading is used TODO: update
