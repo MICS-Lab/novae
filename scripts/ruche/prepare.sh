4632ed6682cb1b045448a510e2e6b789eaf6252d
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2024-08-15 10:54:04 +0200

    prepare on gpu queue

--- scripts/ruche/prepare.sh
+++ scripts/ruche/prepare.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=novae
 #SBATCH --output=/gpfs/workdir/blampeyq/.jobs_outputs/%j
-#SBATCH --mem=80G
+#SBATCH --time=16:00:00
+#SBATCH --partition=gpu
+#SBATCH --mem=160G
 #SBATCH --cpus-per-task=8
-#SBATCH --time=24:00:00
-#SBATCH --partition=cpu_long
+#SBATCH --gres=gpu:1
 
 module purge
 module load anaconda3/2022.10/gcc-11.2.0 && source activate novae
