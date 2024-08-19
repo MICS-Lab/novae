d2a5ca165cf6d69192dcd580506443cd67126866
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2024-08-19 10:48:05 +0200

    fixing pre-commit (add wandb as third_party isort)

--- scripts/utils.py
+++ scripts/utils.py
@@ -6,13 +6,13 @@ import lightning as L
 import numpy as np
 import pandas as pd
 import scanpy as sc
+import wandb
 import yaml
 from anndata import AnnData
 from lightning.pytorch.callbacks import ModelCheckpoint
 from lightning.pytorch.loggers import WandbLogger
 
 import novae
-import wandb
 from novae import log
 from novae._constants import Keys
 from novae.monitor import jensen_shannon_divergence, mean_fide_score, mean_svg_score
