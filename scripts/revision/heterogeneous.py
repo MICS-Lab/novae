4dce4f5caf6721b67bad1c069170790bad8e943b
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2024-12-11 13:37:20 +0100

    add heterogeneous training

---
+++ scripts/revision/heterogeneous.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import scanpy as sc
+
+import novae
+
+adatas = [
+    sc.read_h5ad(
+        "/gpfs/workdir/blampeyq/novae/data/_heterogeneous/Xenium_V1_Human_Colon_Cancer_P2_CRC_Add_on_FFPE_outs.h5ad"
+    ),
+    sc.read_h5ad("/gpfs/workdir/blampeyq/novae/data/_heterogeneous/Xenium_V1_Human_Brain_GBM_FFPE_outs.h5ad"),
+]
+
+adatas[0].uns["novae_tissue"] = "colon"
+adatas[1].uns["novae_tissue"] = "brain"
+
+novae.utils.spatial_neighbors(adatas, radius=80)
+
+model = novae.Novae(adatas, min_prototypes_ratio=0.25)
+
+model.fit(max_epochs=10)
+model.compute_representations()
+
+for level in range(7, 15):
+    model.assign_domains(level=level)
+
+model.plot_prototype_weights()
+plt.savefig("/gpfs/workdir/blampeyq/novae/data/_heterogeneous/prototype_weights.pdf", bbox_inches="tight")
+
+for i, adata in enumerate(adatas):
+    del adata.X
+    for key in adata.layers.keys():
+        del adata.layers[key]
+    adata.write_h5ad(f"/gpfs/workdir/blampeyq/novae/data/_heterogeneous/{i}_res.h5ad")
