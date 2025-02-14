7ba05035e51b76ab43074c47e09040c8e0897554
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2025-02-14 15:46:20 +0100

    try more prototypes

--- scripts/revision/missing_domains.py
+++ scripts/revision/missing_domains.py
@@ -1,33 +1,59 @@
 from pathlib import Path
 
+import matplotlib.pyplot as plt
 import scanpy as sc
 
 import novae
+from novae._constants import Nums
 
-suffix = "_one_latent99"
+Nums.WARMUP_EPOCHS = 3
+
+suffix = "_numproto_0"
 
 path = Path("/gpfs/workdir/blampeyq/novae/data/_lung_robustness")
 
-adata1_full = sc.read_h5ad(path / "v1_full.h5ad")
 adata1_split = sc.read_h5ad(path / "v1_split.h5ad")
 adata2_full = sc.read_h5ad(path / "v2_full.h5ad")
-adata2_split = sc.read_h5ad(path / "v2_split.h5ad")
+# adata2_split = sc.read_h5ad(path / "v2_split.h5ad")
 
-model = novae.Novae.from_pretrained("MICS-Lab/novae-human-0")
 
 adatas = [adata1_split, adata2_full]
 
-model.fine_tune(adatas, min_prototypes_ratio=0.5)
-model.compute_representations(adatas)
+# shared_genes = adata1_split.var_names.intersection(adata2_full.var_names)
+# adata1_split = adata1_split[:, shared_genes].copy()
+# adata2_full = adata2_full[:, shared_genes].copy()
+# adatas = [adata1_split, adata2_full]
 
+model = novae.Novae(
+    adatas,
+    num_prototypes=3000,
+    heads=8,
+    hidden_size=128,
+    min_prototypes_ratio=0.8,
+)
+model.fit()
+model.compute_representations()
+
+# model = novae.Novae.from_pretrained("MICS-Lab/novae-human-0")
+# model.fine_tune(adatas, min_prototypes_ratio=0.5, reference="largest")
+# model.compute_representations(adatas)
+
+obs_key = model.assign_domains(adatas, resolution=1)
+obs_key = model.assign_domains(adatas, resolution=0.5)
 obs_key = model.assign_domains(adatas, level=7)
+
+model.plot_prototype_weights()
+plt.savefig(path / f"prototype_weights{suffix}.pdf", bbox_inches="tight")
+
+# model.umap_prototypes()
+# plt.savefig(path / f"umap_prototypes{suffix}.png", bbox_inches="tight")
 
 for adata in adatas:
     adata.obs[f"{obs_key}_split_ft"] = adata.obs[obs_key]
 
 ### Save
 
-for adata, name in [(adata1_full, "v1_full"), (adata1_split, "v1_split"), (adata2_full, "v2_full")]:
+for adata, name in [(adata1_split, "v1_split"), (adata2_full, "v2_full")]:
     del adata.X
     for key in list(adata.layers.keys()):
         del adata.layers[key]
