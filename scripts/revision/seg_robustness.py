d604fa5db53480b7b9142afa5bee70af03f2e09b
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2025-02-14 15:51:00 +0100

    re-run seg robustness

--- scripts/revision/seg_robustness.py
+++ scripts/revision/seg_robustness.py
@@ -7,13 +7,24 @@ adatas = [
     anndata.read_h5ad("/gpfs/workdir/blampeyq/novae/data/_colon_seg/adata_default_graph.h5ad"),
 ]
 
-model = novae.Novae.from_pretrained("MICS-Lab/novae-human-0")
+suffix = "_2"
 
-model.fine_tune(adatas)
-model.compute_representations(adatas)
+model = novae.Novae(
+    adatas,
+    num_prototypes=3000,
+    heads=8,
+    hidden_size=128,
+    min_prototypes_ratio=1,
+)
+model.fit()
+model.compute_representations()
+
+# model = novae.Novae.from_pretrained("MICS-Lab/novae-human-0")
+# model.fine_tune(adatas)
+# model.compute_representations(adatas)
 
 for level in range(7, 15):
     model.assign_domains(adatas, level=level)
 
-adatas[0].write_h5ad("/gpfs/workdir/blampeyq/novae/data/_colon_seg/adata_graph_domains.h5ad")
-adatas[1].write_h5ad("/gpfs/workdir/blampeyq/novae/data/_colon_seg/adata_default_graph_domains.h5ad")
+adatas[0].write_h5ad(f"/gpfs/workdir/blampeyq/novae/data/_colon_seg/adata_graph_domains{suffix}.h5ad")
+adatas[1].write_h5ad(f"/gpfs/workdir/blampeyq/novae/data/_colon_seg/adata_default_graph_domains{suffix}.h5ad")
