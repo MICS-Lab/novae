93bfa3dab285df3aac7b5f300051de8112cb86ae
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2025-02-18 09:46:33 +0100

    try join

--- scripts/revision/missing_domains.py
+++ scripts/revision/missing_domains.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import anndata
 import matplotlib.pyplot as plt
 import scanpy as sc
 
@@ -7,9 +8,9 @@ import novae
 from novae._constants import Nums
 
 Nums.WARMUP_EPOCHS = 1
-Nums.SWAV_EPSILON = 0.01
+# Nums.SWAV_EPSILON = 0.01
 
-suffix = "_numproto_3"
+suffix = "_numproto_4"
 
 path = Path("/gpfs/workdir/blampeyq/novae/data/_lung_robustness")
 
@@ -17,8 +18,8 @@ adata1_split = sc.read_h5ad(path / "v1_split.h5ad")
 adata2_full = sc.read_h5ad(path / "v2_full.h5ad")
 # adata2_split = sc.read_h5ad(path / "v2_split.h5ad")
 
-
-adatas = [adata1_split, adata2_full]
+adata_join = anndata.concat([adata1_split, adata2_full], join="inner")
+# adatas = [adata1_split, adata2_full]
 
 # shared_genes = adata1_split.var_names.intersection(adata2_full.var_names)
 # adata1_split = adata1_split[:, shared_genes].copy()
@@ -26,22 +27,22 @@ adatas = [adata1_split, adata2_full]
 # adatas = [adata1_split, adata2_full]
 
 model = novae.Novae(
-    adatas,
+    adata_join,
     num_prototypes=2000,
     heads=8,
     hidden_size=128,
     min_prototypes_ratio=0.5,
 )
-model.fit(lr=1e-4, max_epochs=50, min_delta=0.05)
+model.fit(lr=5e-4, max_epochs=30, min_delta=0.05)
 model.compute_representations()
 
 # model = novae.Novae.from_pretrained("MICS-Lab/novae-human-0")
 # model.fine_tune(adatas, min_prototypes_ratio=0.5, reference="largest")
 # model.compute_representations(adatas)
 
-obs_key = model.assign_domains(adatas, resolution=1)
-obs_key = model.assign_domains(adatas, resolution=0.5)
-obs_key = model.assign_domains(adatas, level=7)
+obs_key = model.assign_domains(adata_join, resolution=1)
+obs_key = model.assign_domains(adata_join, resolution=0.5)
+obs_key = model.assign_domains(adata_join, level=7)
 
 model.plot_prototype_weights()
 plt.savefig(path / f"prototype_weights{suffix}.pdf", bbox_inches="tight")
@@ -49,12 +50,13 @@ plt.savefig(path / f"prototype_weights{suffix}.pdf", bbox_inches="tight")
 # model.umap_prototypes()
 # plt.savefig(path / f"umap_prototypes{suffix}.png", bbox_inches="tight")
 
-for adata in adatas:
-    adata.obs[f"{obs_key}_split_ft"] = adata.obs[obs_key]
+adatas = [adata_join[adata_join.obs["novae_sid"] == ID] for ID in adata_join.obs["novae_sid"].unique()]
+names = ["v1_split", "v2_full"]
 
 ### Save
 
-for adata, name in [(adata1_split, "v1_split"), (adata2_full, "v2_full")]:
+for adata, name in zip(adatas, names):
+    adata.obs[f"{obs_key}_split_ft"] = adata.obs[obs_key]
     del adata.X
     for key in list(adata.layers.keys()):
         del adata.layers[key]
