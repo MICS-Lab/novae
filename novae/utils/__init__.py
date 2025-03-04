29cd08d4261d0d288a3a7891449b2fe2f6303405
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2025-03-04 15:15:44 +0100

    fix init utils

--- novae/utils/__init__.py
+++ novae/utils/__init__.py
@@ -1,27 +1,22 @@
-from ._build import spatial_neighbors
-from ._correct import batch_effect_correction
-from ._data import load_dataset, load_local_dataset, load_wandb_artifact, toy_dataset
-from ._mode import Mode
-from ._preprocess import quantile_scaling
 from ._utils import (
-    fill_invalid_indices,
-    get_relative_sensitivity,
-    iter_slides,
-    lower_var_names,
-    parse_device_args,
-    pretty_model_repr,
-    pretty_num_parameters,
     repository_root,
-    sparse_std,
     tqdm,
+    fill_invalid_indices,
+    lower_var_names,
+    wandb_log_dir,
+    pretty_num_parameters,
+    pretty_model_repr,
+    parse_device_args,
+    valid_indices,
     unique_leaves_indices,
     unique_obs,
-    valid_indices,
-    wandb_log_dir,
+    sparse_std,
+    iter_slides,
+    get_relative_sensitivity,
 )
-from ._validate import (
-    check_available_domains_key,
-    check_has_spatial_adjancency,
-    check_slide_name_key,
-    prepare_adatas,
-)
+from ._build import spatial_neighbors
+from ._validate import check_available_domains_key, prepare_adatas, check_has_spatial_adjancency, check_slide_name_key
+from ._data import load_local_dataset, load_wandb_artifact, toy_dataset, load_dataset
+from ._mode import Mode
+from ._correct import batch_effect_correction
+from ._preprocess import quantile_scaling
