79bec99ef22eea4c602ce181729b59da448a3e26
Quentin Blampey <quentin.blampey@gmail.com>
2025-04-23 15:53:26 +0200

    minor cleanup

--- novae/utils/_histo.py
+++ novae/utils/_histo.py
@@ -29,21 +29,22 @@ def compute_he_embeddings(
     assert 0 <= patch_overlap_ratio < 1, "patch_overlap_ratio must be between 0 and 1"
     patch_overlap = int(patch_overlap_ratio * Nums.HE_PATCH_WIDTH)
 
+    adata: AnnData = sdata[table_key]
+
     if image_key is None:
         image_key, _ = sopa.utils.get_spatial_element(
             sdata.images, sdata.attrs.get(SopaAttrs.TISSUE_SEGMENTATION, None), return_key=True
         )
 
-    adata: AnnData = sdata[table_key]
-
+    # align the cells boundaries to the image coordinate system
     shapes_key, _, instance_key = get_table_keys(adata)
-
     cells = sopa.utils.to_intrinsic(sdata, shapes_key, image_key)
     cells = cells.loc[adata.obs[instance_key]]
 
     sopa.patches.compute_embeddings(
         sdata,
         model,
+        level=0,
         patch_width=Nums.HE_PATCH_WIDTH,
         patch_overlap=patch_overlap,
         image_key=image_key,
@@ -54,8 +55,6 @@ def compute_he_embeddings(
     patches_centroids = sdata[SopaKeys.EMBEDDINGS_PATCHES].centroid
     closest_patch_index = patches_centroids.sindex.nearest(cells.centroid, return_all=False)[1]
 
-    cells_he_embeddings = sdata.tables[f"{model}_embeddings"].X[closest_patch_index]
-
-    adata.obsm[Keys.HISTO_EMBEDDINGS] = cells_he_embeddings
+    adata.obsm[Keys.HISTO_EMBEDDINGS] = sdata.tables[f"{model}_embeddings"].X[closest_patch_index]
 
     return adata
