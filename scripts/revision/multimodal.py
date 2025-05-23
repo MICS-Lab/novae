1d1e809dcec8a91a555ea9e6ac5b3a6c44db171d
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2025-05-23 12:21:26 +0200

    prepare multimodal bench

---
+++ scripts/revision/multimodal.py
@@ -0,0 +1,34 @@
+import scanpy as sc
+from sklearn.preprocessing import StandardScaler
+
+import novae
+
+
+def main():
+    adata = sc.read_h5ad("/gpfs/workdir/blampeyq/data/adata_lung_conch_cells.h5ad")
+    novae.spatial_neighbors(adata)
+
+    scaler = StandardScaler()
+    adata.obsm["histo_embeddings"] = scaler.fit_transform(adata.obsm["histo_embeddings"])
+
+    model = novae.Novae(adata)
+    model.fit(adata, accelerator="cuda", num_workers=4)
+    model.compute_representations(adata, accelerator="cuda", num_workers=4)
+
+    model.assign_domains(adata, level=8)
+
+    adata.write_h5ad("/gpfs/workdir/blampeyq/res_novae/adata_lung_conch.h5ad")
+
+    del adata.obsm["histo_embeddings"]
+
+    model = novae.Novae(adata)
+    model.fit(adata, accelerator="cuda", num_workers=4)
+    model.compute_representations(adata, accelerator="cuda", num_workers=4)
+
+    model.assign_domains(adata, level=8)
+
+    adata.write_h5ad("/gpfs/workdir/blampeyq/res_novae/adata_lung_no_conch.h5ad")
+
+
+if __name__ == "__main__":
+    main()
