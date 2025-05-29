a489232e622469932fbff4dff15ae7c6da9a18fd
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2025-05-29 18:00:12 +0200

    script for artif batch effect expes

---
+++ scripts/revision/artif_batch_effect.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+
+import scanpy as sc
+
+import novae
+
+DIR = Path("/gpfs/workdir/shared/prime/spatial/inter_batch_effect_simulation")
+
+names = ["lung_st", "brain_st"]
+domains = [7, 15]
+
+
+def main():
+    for domain, name in zip(domains, names):
+        adatas = [sc.read_h5ad(DIR / name / f"{name}.h5ad")] + [
+            sc.read_h5ad(DIR / name / f"{name}_level_{i}.h5ad") for i in range(1, 6)
+        ]
+
+        novae.utils.spatial_neighbors(adatas, radius=80)
+
+        model = novae.Novae(adatas)
+
+        model.fit(accelerator="cuda", num_workers=4)
+        model.compute_representations(accelerator="cuda", num_workers=8)
+
+        obs_key = model.assign_domains(adatas, n_domains=domain)
+
+        adata_reference = adatas[0]
+
+        scores = []
+        for adata in adatas[1:]:
+            gt = adata_reference.obs[obs_key]
+            pred = adata.obs[obs_key]
+
+            keep = ~gt.isna()
+            accuracy = (gt[keep] == pred[keep]).mean()
+            scores.append(accuracy)
+
+        for i, adata in enumerate(adatas):
+            if i > 0:
+                del adata.X
+
+            adata.write_h5ad(f"/gpfs/workdir/shared/prime/spatial/temp/{name}_level_{i}_domains.h5ad")
+
+        print(f"Domain: {domain}, Name: {name}, Scores: {scores}")
+
+
+if __name__ == "__main__":
+    main()
