11ade9e585ad545cdfb17bbc5e8e023070d1f3c1
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2025-06-17 10:57:24 +0200

    new fig + re-run with defaults

--- scripts/revision/prepare_tuto_plot.py
+++ scripts/revision/prepare_tuto_plot.py
@@ -18,11 +18,19 @@ def main():
 
     novae.compute_histo_pca(sdata)
 
-    adata = sdata["table"]
+    adata = sc.read_h5ad("/gpfs/workdir/blampeyq/res_novae/tuto_pp.h5ad")
 
     adata.obs["pca_dim0_cells"] = adata.obsm["histo_embeddings"][:, 0]
     sc.pl.spatial(adata, color="pca_dim0_cells", spot_size=10, show=False)
     plt.savefig("/gpfs/workdir/blampeyq/res_novae/conch_emb_projected.png", bbox_inches="tight")
+
+    model = novae.Novae(adata)
+    model.fit(adata, accelerator="cuda", num_workers=8)
+    model.compute_representations(adata, accelerator="cuda", num_workers=8)
+
+    model.assign_domains(adata, level=8)
+
+    adata.write_h5ad("/gpfs/workdir/blampeyq/res_novae/tuto_res2.h5ad")
 
 
 if __name__ == "__main__":
