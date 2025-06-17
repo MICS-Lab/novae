08f341b93cc206eaf791e7794b312d70e9825434
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2025-06-17 11:38:28 +0200

    only last part

--- scripts/revision/prepare_tuto_plot.py
+++ scripts/revision/prepare_tuto_plot.py
@@ -1,22 +1,21 @@
 import matplotlib.pyplot as plt
 import scanpy as sc
-import spatialdata
 
 import novae
 
 
 def main():
-    output_file = "/gpfs/workdir/shared/prime/spatial/tuto.zarr"
+    # output_file = "/gpfs/workdir/shared/prime/spatial/tuto.zarr"
 
-    sdata = spatialdata.read_zarr(output_file)
-    print(sdata)
+    # sdata = spatialdata.read_zarr(output_file)
+    # print(sdata)
 
-    adata_conch = sdata["conch_embeddings"]
+    # adata_conch = sdata["conch_embeddings"]
 
-    sc.pl.spatial(adata_conch, color="0", spot_size=10, show=False)
-    plt.savefig("/gpfs/workdir/blampeyq/res_novae/conch_emb.png", bbox_inches="tight")
+    # sc.pl.spatial(adata_conch, color="0", spot_size=10, show=False)
+    # plt.savefig("/gpfs/workdir/blampeyq/res_novae/conch_emb.png", bbox_inches="tight")
 
-    novae.compute_histo_pca(sdata)
+    # novae.compute_histo_pca(sdata)
 
     adata = sc.read_h5ad("/gpfs/workdir/blampeyq/res_novae/tuto_pp.h5ad")
 
