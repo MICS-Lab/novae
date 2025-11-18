a1033c75a86f4de2cf3a870b5dbdc61f47b69461
Quentin Blampey <quentin.blampey@gmail.com>
2025-11-18 14:49:32 +0100

    support dask backend

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -15,16 +15,37 @@ x_sparse = csr_matrix(x_np)
 x_dask = da.from_array(x_np)
 
 
-def test_backends():
+def test_backends_mean_var():
     mean_np, std_np = np.mean(x_np, axis=0), np.std(x_np, axis=0)
 
     fau_mean_np, fau_var_np = stats.mean_var(x_np, axis=0)
     fau_mean_sparse, fau_var_sparse = stats.mean_var(x_sparse, axis=0)
+    fau_mean_dask, fau_var_dask = stats.mean_var(x_dask, axis=0)
 
     assert np.allclose(mean_np, fau_mean_np)
     assert np.allclose(std_np, fau_var_np**0.5)
     assert np.allclose(mean_np, fau_mean_sparse)
     assert np.allclose(std_np, fau_var_sparse**0.5)
+    assert np.allclose(mean_np, fau_mean_dask)
+    assert np.allclose(std_np, fau_var_dask**0.5)
+
+
+def test_backends_min_max():
+    min_np, max_np = np.min(x_np, axis=0), np.max(x_np, axis=0)
+
+    fau_min_np = stats.min(x_np, axis=0)
+    fau_min_sparse = stats.min(x_sparse, axis=0)
+    fau_min_dask = stats.min(x_dask, axis=0)
+    fau_max_np = stats.max(x_np, axis=0)
+    fau_max_sparse = stats.max(x_sparse, axis=0)
+    fau_max_dask = stats.max(x_dask, axis=0)
+
+    assert np.allclose(min_np, fau_min_np)
+    assert np.allclose(max_np, fau_max_np)
+    assert np.allclose(min_np, fau_min_sparse)
+    assert np.allclose(max_np, fau_max_sparse)
+    assert np.allclose(min_np, fau_min_dask)
+    assert np.allclose(max_np, fau_max_dask)
 
 
 def test_spatially_variable_genes_backend():
