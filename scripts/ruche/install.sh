fc1b11d7f7a638cab60aa02c4a6874a5efaad97a
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2026-01-30 19:59:48 +0100

    update install

--- scripts/ruche/install.sh
+++ scripts/ruche/install.sh
@@ -16,9 +16,10 @@ module load cuda/12.0.0/gcc-11.2.0
 
 conda install anaconda::pyzmq -y
 conda install conda-forge::pyarrow -y
+conda install conda-forge::h5py -y
 
-pip install git+https://github.com/theislab/scConcept.git@main
 pip install git+https://github.com/theislab/lamin_dataloader.git
+pip install git+https://github.com/theislab/scConcept.git@main
 pip install flash-attn==2.7.* --no-build-isolation
 
 cd /gpfs/workdir/blampeyq/novae
