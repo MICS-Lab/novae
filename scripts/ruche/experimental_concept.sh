a49b3c78502d8f7bcd37a3f7de21fc4ad5fd33fd
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2026-02-03 17:40:59 +0100

    add exp concept

---
+++ scripts/ruche/experimental_concept.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=novae
+#SBATCH --output=/gpfs/workdir/blampeyq/.jobs_outputs/%j
+#SBATCH --time=24:00:00
+#SBATCH --partition=gpu
+#SBATCH --mem=300G
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:1
+
+module purge
+
+module load anaconda3/2024.06/gcc-13.2.0
+module load gcc/13.2.0/gcc-4.8.5
+module load binutils/2.43.1/gcc-13.2.0
+module load cmake/3.28.3/gcc-11.2.0
+module load openblas/0.3.8/gcc-9.2.0
+module load cuda/12.0.0/gcc-11.2.0
+
+source activate concept
+
+cd /gpfs/workdir/blampeyq/novae/scripts/experimental
+
+python -u $@
