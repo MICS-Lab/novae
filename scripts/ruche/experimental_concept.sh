7d737b457b12d14b49660435f9e258bb37199731
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2026-02-06 15:34:09 +0100

    use gpua100

--- scripts/ruche/experimental_concept.sh
+++ scripts/ruche/experimental_concept.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=novae
 #SBATCH --output=/gpfs/workdir/blampeyq/.jobs_outputs/%j
 #SBATCH --time=24:00:00
-#SBATCH --partition=gpu
+#SBATCH --partition=gpua100
 #SBATCH --mem=300G
 #SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:1
