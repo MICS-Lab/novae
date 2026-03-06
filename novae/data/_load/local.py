59bc602a62bfeaddeabe2be08cd3a442127d8629
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2026-03-06 11:54:04 +0100

    add white list

--- novae/data/_load/local.py
+++ novae/data/_load/local.py
@@ -1,24 +1,25 @@
 import logging
+from pathlib import Path
 
 import anndata
+import pandas as pd
 from anndata import AnnData
 
-from ...utils import repository_root
+from ..._constants import Keys
 
 log = logging.getLogger(__name__)
 
 
-def load_local_dataset(relative_path: str, files_black_list: list[str] | None = None) -> list[AnnData]:
-    """Load one or multiple AnnData objects based on a relative path from the data directory
+def load_local_dataset(path: str, files_black_list: list[str] | None = None) -> list[AnnData]:
+    """Load one or multiple AnnData objects based on a relative path from the data directory.
 
     Args:
-        relative_path: Relative from from the data directory. If a directory, loads every .h5ad files inside it. Can also be a file, or a file pattern.
+        path: Path to the data directory. If a directory, loads every .h5ad files inside it. Can also be a single file. If a directory, you can add a `white_list.csv` file to mention slides to keep (have a novae_sid and novae_tissue column).
 
     Returns:
         A list of AnnData objects
     """
-    data_dir = repository_root() / "data"
-    full_path = data_dir / relative_path
+    full_path = Path(path)
 
     files_black_list = files_black_list or []
 
@@ -27,9 +28,21 @@ def load_local_dataset(relative_path: str, files_black_list: list[str] | None = 
         log.info(f"Loading one adata: {full_path}")
         return [anndata.read_h5ad(full_path)]
 
-    all_paths = list(data_dir.glob(relative_path) if ".h5ad" in relative_path else full_path.glob("*.h5ad"))
+    all_paths = [path for path in full_path.glob("*.h5ad") if path.name not in files_black_list]
 
-    all_paths = [path for path in all_paths if path.name not in files_black_list]
+    selection = None
+    white_list_path: Path = path / "white_list.csv"
+    if white_list_path.exists():
+        selection = pd.read_csv(white_list_path, index_col=Keys.SLIDE_ID)
+        all_paths = [path for path in all_paths if path.stem in selection.index]
 
     log.info(f"Loading {len(all_paths)} adata(s): {', '.join([str(path) for path in all_paths])}")
-    return [anndata.read_h5ad(path) for path in all_paths]
+    adatas = []
+
+    for path in all_paths:
+        adata = anndata.read_h5ad(path)
+        if selection is not None:
+            adata.uns[Keys.UNS_TISSUE] = selection.loc[path.stem, Keys.UNS_TISSUE]
+        adatas.append(adata)
+
+    return adatas
